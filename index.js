9cf0fe28b397c03bb1182469e115e5384b699353
Ussen_Kimanuka <ukimanuka@gmail.com>
2023-10-01 20:04:19 +0300

    working with strings

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = 'USSEN';
+// let currentUser = 'Ussen Kimanuka';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser}!`;
+const shortGreeting = `Welcome, U!`
